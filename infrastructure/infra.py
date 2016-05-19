9add2a17ef93c02a7bcb0c8c30c5b3d86be24645
austburn <austburn@gmail.com>
2016-05-19 09:13:57 -0400

    Add the start of the docker pipeline

--- infrastructure/infra.py
+++ infrastructure/infra.py
@@ -12,18 +12,20 @@ app.logger.addHandler(handler)
 @app.route('/post', methods=['POST'])
 def post():
     json_body = request.get_json()
+    Repo.clone_from('git@github.com:austburn/austburn.me.git', '/data/')
+    cli = Client(base_url='unix://var/run/docker.sock')
+
+    with f as open('/data/docker/app.docker'):
+        try:
+            response = [
+                line for line in cli.build(
+                    fileobj=f, path='/data/', tag='austburn.app'
+                )
+            ]
+        except Exception as e:
+            app.logger.exception(e)
+
     app.logger.info(json_body)
+
     return '', 200
 
-# Repo.clone_from('git@github.com:austburn/austburn.me.git', '/home/austin/austburn.me')
-# cli = Client(base_url='unix://var/run/docker.sock')
-
-# with f as open('/home/austin/austburn.me/docker/app.docker'):
-#     try:
-#         response = [
-#             line for line in cli.build(
-#                 fileobj=f, path='/home/austin/austburn.me', tag='austburn.app'
-#             )
-#         ]
-#     except Exception:
-#         # ah
