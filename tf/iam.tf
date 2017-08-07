b5689750154d1c71f1d09d71d68cee90be0f9129
Austin Burnett <austburn@gmail.com>
2017-08-06 20:41:46 -0500

    Update permissions and add terraform apply

--- tf/iam.tf
+++ tf/iam.tf
@@ -120,7 +120,11 @@ resource "aws_iam_user_policy" "circle_ci_push" {
         "ecr:InitiateLayerUpload",
         "ecr:UploadLayerPart",
         "ecr:CompleteLayerUpload",
-        "ecr:BatchCheckLayerAvailability"
+        "ecr:BatchCheckLayerAvailability",
+        "ec2:*",
+        "ecs:*",
+        "route53:*",
+        "vpc:*"
       ],
       "Effect": "Allow",
       "Resource": "*"
